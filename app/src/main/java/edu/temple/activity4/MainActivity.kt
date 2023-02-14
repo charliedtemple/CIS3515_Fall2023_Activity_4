5d7b8674c31902c18847e569e4f4541877a7a05f
Charlie Dunford <tum08940@temple.edu>
2023-02-14 11:55:19 -0500

    Added recyclerView layout and code for TextSizeAdaoter

--- app/src/main/java/edu/temple/activity4/MainActivity.kt
+++ app/src/main/java/edu/temple/activity4/MainActivity.kt
@@ -2,6 +2,9 @@ package edu.temple.activity4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -15,13 +18,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Trying to create array of integers that are multiples of 5
+        textSizeDisplay = findViewById(R.id.textSizeDisplayTextView)
+        textSizeSelector = findViewById(R.id.recyclerView)
         // Verify correctness by examining array values.
         val textSizes = Array(20){(it + 1) * 5}
+        for(i in 1..textSizes.size)
+            Log.d("Array values", textSizes[i].toString())
     }
 }
 
 
 /* Convert to RecyclerView.Adapter */
-class TextSizeAdapter {
+class TextSizeAdapter : RecyclerView.Adapter<TextSizeAdapter.TextSizeViewHolder>() {
+    class TextSizeViewHolder(view:View) : RecyclerView.ViewHolder(view){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextSizeViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: TextSizeViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 
 }
